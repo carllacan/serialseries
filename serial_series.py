b465061d0dd341f9d8b065b17bf8db384ee6d5cd
Carles Llàcer <carles.llacer@gmail.com>
2018-06-01 15:54:07 +0200

    Cleaning.

--- serial_series.py
+++ serial_series.py
@@ -23,7 +23,7 @@ def read_and_print(serial):
         print(resp)
         
 
-def runcommands(serial, cs, ts, verbose = False):
+def runcommands(cs, ts, ss, verbose = False):
     """ Runs a series of commands at certain specified times """
     if len(ts) == len(cs):
         i = 0
@@ -32,12 +32,12 @@ def runcommands(serial, cs, ts, verbose = False):
         while i < len(cs):
             while (dt - ts[i]) < 0.0005:
                 dt = time.time() - t0
-                if verbose: read_and_print(serial)
+                if verbose: read_and_print(ss[i])
 #            if verbose: print("Time: {:2.3f}".format(dt))
-            write_command(serial, cs[i], verbose)
+            write_command(ss[i], cs[i], verbose)
             i += 1
     else:
-        print('Error: Times and commands lists are not equally long. ')
+        print('Error: Lists are not equally long. ')
 
 
 
@@ -62,21 +62,15 @@ ts = [0.000,
       0.040,
       0.080,
       1.040,
-      6.080]
+      1.080]
 
+ps = ["COM1",
+      "COM1",
+      "COM1",
+      "COM1",
+      "COM1"]
 
-
-#cs = ["F", 
-#      "L",
-#      "F",
-#      "L",
-#      ""]
-#
-#ts = [0.000,
-#      0.040,
-#      0.080,
-#      0.100,
-#      1.000]
+ports = list(set(ps))
 
 slave = False
 
@@ -87,15 +81,8 @@ for r in range(reps):
     for t in ts:
         ts_rep.append(t + ts[-1]*r)
 
-if slave: write_command(ser, "m", verbose=True)
-#time.sleep(0.1)
-#ftn.read_print()
 
-#for i in range(reps):
-#    ftn.runcommands(cs, ts)
 runcommands(ser, cs_rep, ts_rep, verbose=True)
     
-time.sleep(0.5)
-if slave: write_command(ser, "m", verbose=True)
 time.sleep(0.5)
 ser.close()
